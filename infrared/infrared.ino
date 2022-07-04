9a37ff8413ab5927bef1ac72162ca7e10eee4f6b
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-04 15:26:25 +0800

    Initial commit

--- infrared/infrared.ino
+++ infrared/infrared.ino
@@ -1,6 +1,7 @@
 int OutputPin = 5;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(OutputPin,INPUT);
 }
 
--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -1,7 +1,9 @@
+#include <analogWrite.h> 
 int Signal = 18;
 int PowerPin = 19;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(Signal,INPUT);
   pinMode(PowerPin,OUTPUT);
 }
