b8d91401d0c9082d80ea544e912b4bc6a01352ca
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-04 15:36:09 +0800

    Initial commit

--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -1,11 +1,12 @@
 #include <analogWrite.h> 
-int Signal = 18;
-int PowerPin = 19;
+int OutPin = 18;
+int ADJ_Pin = 19;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(Signal,INPUT);
-  pinMode(PowerPin,OUTPUT);
+  pinMode(OutPin,INPUT);
+  pinMode(ADJ_Pin,INPUT);
+  int water_level = 0;
 }
 
 void loop() {
