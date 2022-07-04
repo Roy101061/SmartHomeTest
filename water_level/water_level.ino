0478a0c6ee34b65c65566819c7fcad056e265713
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-04 15:41:48 +0800

    Update water_level.ino

--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -5,7 +5,7 @@ int ADJ_Pin = 19;
 void setup() {
   Serial.begin(115200);
   pinMode(OutPin,INPUT);
-  pinMode(ADJ_Pin,INPUT);
+  pinMode(ADJ_Pin,INPUT); // Wondering how to use or useless.
   int Water_level = 0;
 }
 
@@ -14,7 +14,7 @@ void loop() {
   Serial.print("Water_level= ");
   Serial.println(Water_level);
   if(Water_level!=0){
-    
+
   }
   delay(30000); //delay 30s
 }
