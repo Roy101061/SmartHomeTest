ef143249fa3d201e24d11da131d9f2563adb5157
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-05 15:29:43 +0800

    Update infrared_send.ino

--- infrared_send/infrared_send.ino
+++ infrared_send/infrared_send.ino
@@ -11,35 +11,45 @@ void setup() {
 
 void loop() {
   int chose;
+  int type;
   if( (chose = Serial.read()) != -1) {
   unsigned long code = 0x0;
    switch(chose){
       case '1':
         code = 0xFFA25D;
+        type = 8;
       break;
       case '2':
         code = 0xFF629D;
+        type = 8;
       break;
       case '3':
         code = 0xFFE21D;
+        type = 8;
       break;
       case '4':
         code = 0xFF22DD;
+        type = 8;
       break;
       case '5':
         code = 0xFF02FD;
+        type = 8;
       break;
       case '6':
         code = 0xFFC23D;
+        type = 8;
       break;
       case '7':
         code = 0xFFE01F;
+        type = 8;
       break;
       case '8':
         code = 0xFFA857;
+        type = 8;
       break;
       case '9':
         code = 0xFF906F;
+        type = 8;
       break;
     }
   
@@ -48,7 +58,40 @@ void loop() {
       Serial.print(chose);
       Serial.print(", IR send ");
       Serial.println(code, HEX);
-      irsend.sendJVC(code, 32);
+      if(type!=-1){
+        switch(type){
+          case '1':
+            irsend.sendNEC(code, 32);
+          break;
+          case '2':
+            irsend.sendSony(code, 32);
+          break;
+          case '3':
+            irsend.sendRC5(code, 32);
+          break;
+          case '4':
+            irsend.sendRC6(code, 32);
+          break;
+          case '5':
+            irsend.sendDISH(code, 32);
+          break;
+          case '6':
+            irsend.sendSharp(code, 32);
+          break;
+          case '7':
+            irsend.sendPanasonic(code, 32);
+          break;
+          case '8':
+            irsend.sendJVC(code, 32);
+          break;
+          case '9':
+            irsend.sendSanyo(code, 32);
+          break;
+          default :
+            irsend.sendMitsubishi(code, 32);
+          break;
+        }
+      }
     }
   }
 }
