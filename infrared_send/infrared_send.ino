fdb5f8aeda145a7117342dc80c48a5133d56ad6f
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-06 14:32:30 +0800

    Update infrared_send.ino

--- infrared_send/infrared_send.ino
+++ infrared_send/infrared_send.ino
@@ -11,84 +11,76 @@ void setup() {
 
 void loop() {
   int chose;
-  int type;
+  int Code;
+  int type = 0; //NEC = 1 , SONY = 2 , RC5 = 3 , RC6 = 4 , UNKNOWN = 5
+  unsigned long Address;
+  unsigned long Command;
   if( (chose = Serial.read()) != -1) {
-  unsigned long code = 0x0;
    switch(chose){
       case '1':
-        code = 0xFFA25D;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x45;
       break;
       case '2':
-        code = 0xFF629D;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x46;
       break;
       case '3':
-        code = 0xFFE21D;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x47;
       break;
       case '4':
-        code = 0xFF22DD;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x44;
       break;
       case '5':
-        code = 0xFF02FD;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x40;
       break;
       case '6':
-        code = 0xFFC23D;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x43;
       break;
       case '7':
-        code = 0xFFE01F;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x7;
       break;
       case '8':
-        code = 0xFFA857;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x15;
       break;
       case '9':
-        code = 0xFF906F;
-        type = 8;
+        Address = 0x0;
+        type = 1;
+        Command = 0x9;
       break;
     }
   
-  if(code != 0x0){
-      Serial.print("read ");
-      Serial.print(chose);
-      Serial.print(", IR send ");
-      Serial.println(code, HEX);
-      if(type!=-1){
+  if(type != 0){
+      if(type!=0){
         switch(type){
           case '1':
-            irsend.sendNEC(code, 32);
+            IrSender.sendNEC(Address, Command, true, 0);
           break;
           case '2':
-            irsend.sendSony(code, 32);
+            IrSender.sendSONY(Address, Command, true, 0);
           break;
           case '3':
-            irsend.sendRC5(code, 32);
+            IrSender.sendRC5(Address, Command, true, 0);
           break;
           case '4':
-            irsend.sendRC6(code, 32);
-          break;
-          case '5':
-            irsend.sendDISH(code, 32);
-          break;
-          case '6':
-            irsend.sendSharp(code, 32);
-          break;
-          case '7':
-            irsend.sendPanasonic(code, 32);
-          break;
-          case '8':
-            irsend.sendJVCMSB(code, 32);
-          break;
-          case '9':
-            //irsend.sendSanyo(code, 32);
+            IrSender.sendRC6(Address, Command, true, 0);
           break;
           default :
-            //irsend.sendKaseikyo_Mitsubishi(code, 32);
+            IrSender.sendNECRaw(Code);
           break;
         }
       }
