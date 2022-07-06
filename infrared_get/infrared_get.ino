a62b04adb6039beb6b48d698011c4d1cc8c2a88f
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-06 11:35:11 +0800

    Update infrared_get.ino

--- infrared_get/infrared_get.ino
+++ infrared_get/infrared_get.ino
@@ -1,25 +1,18 @@
 #include <IRremote.h>
 int SignalPin = 5;
 int ControllerPin = 18;
-IRrecv irrecv(SignalPin);
-decode_results results;
 
 void setup() {
   Serial.begin(115200);
 	pinMode(SignalPin,INPUT);
 	pinMode(ControllerPin,OUTPUT);
-  irrecv.blink13(true);
-	irrecv.enableIRIn(); 
+  IrReceiver.begin(SignalPin, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.print("results value is ");
-    Serial.print(results.value, HEX);
-    Serial.print(", bits is ");
-    Serial.print(results.bits);
-    Serial.print(", decode_type is ");
-    Serial.println(results.decode_type);
-    irrecv.resume();
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.resume();
   }
 }
