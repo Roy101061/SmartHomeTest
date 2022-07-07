7b37965d3d969e4b1d2d41324bce079b3c961b49
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-07 14:13:23 +0800

    Update infrared_send.ino

--- infrared_send/infrared_send.ino
+++ infrared_send/infrared_send.ino
@@ -21,73 +21,80 @@ void loop() {
         Address = 0x0;
         type = 1;
         Command = 0x45;
+        Serial.println("chose1");
       break;
       case '2':
         Address = 0x0;
-        type = 1;
+        type = 2;
         Command = 0x46;
+        Serial.println("chose2");
       break;
       case '3':
         Address = 0x0;
-        type = 1;
+        type = 3;
         Command = 0x47;
+        Serial.println("chose3");
       break;
       case '4':
         Address = 0x0;
-        type = 1;
+        type = 4;
         Command = 0x44;
+        Serial.println("chose4");
       break;
       case '5':
         Address = 0x0;
-        type = 1;
+        type = 5;
         Command = 0x40;
+        Serial.println("chose5");
       break;
       case '6':
         Address = 0x0;
         type = 1;
         Command = 0x43;
+        Serial.println("chose6");
       break;
       case '7':
         Address = 0x0;
         type = 1;
         Command = 0x7;
+        Serial.println("chose7");
       break;
       case '8':
         Address = 0x0;
         type = 1;
         Command = 0x15;
+        Serial.println("chose8");
       break;
       case '9':
         Address = 0x0;
         type = 1;
         Command = 0x9;
+        Serial.println("chose9");
       break;
     }
-  
+  Serial.println(type);
   if(type != 0){
-      if(type!=0){
-        switch(type){
-          case '1':
-            IrSender.sendNEC(Address, Command, true, 0);
-            Serial.println("send check1!");
-          break;
-          case '2':
-            IrSender.sendSony(Address, Command, true, 0);
-            Serial.println("send check2!");
-          break;
-          case '3':
-            IrSender.sendRC5(Address, Command, true, 0);
-            Serial.println("send check3!");
-          break;
-          case '4':
-            IrSender.sendRC6(Address, Command, true, 0);
-            Serial.println("send check4!");
-          break;
-          default :
-            IrSender.sendNECRaw(Code);
-            Serial.println("send check5!");
-          break;
-        }
+      switch(type){
+        case 1:
+          IrSender.sendNEC(Address, Command, true, 0);
+          Serial.println("send check1!");
+        break;
+        case 2:
+          IrSender.sendSony(Address, Command, true, 0);
+          Serial.println("send check2!");
+        break;
+        case 3:
+          IrSender.sendRC5(Address, Command, true, 0);
+          Serial.println("send check3!");
+        break;
+        case 4:
+          IrSender.sendRC6(Address, Command, true, 0);
+          Serial.println("send check4!");
+        break;
+        default :
+          IrSender.sendNECRaw(Code);
+          Serial.println("send check5!");
+        break;
       }
     }
   }
