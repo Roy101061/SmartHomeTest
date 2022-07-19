a30605831c634dc9c831816d558be448ac406d6f
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-07-19 12:03:13 +0800

    Update water_level.ino

--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -1,20 +1,18 @@
-#include <analogWrite.h> 
+
 int OutPin = 18;
-int ADJ_Pin = 19;
 
 void setup() {
   Serial.begin(115200);
   pinMode(OutPin,INPUT);
-  pinMode(ADJ_Pin,INPUT); // Wondering how to use or useless.
-  int Water_level = 0;
 }
 
 void loop() {
-  Water_level = digitalRead(OutPin);
+  int Water_level = 0;
+  Water_level = analogRead(OutPin);
   Serial.print("Water_level= ");
   Serial.println(Water_level);
   if(Water_level!=0){
 
   }
-  delay(30000); //delay 30s
+  delay(1000); //delay 1s
 }
